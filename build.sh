634e6b1a75e846299e1adb4080e15f5234c325d1
yunnysunny <yunnysunny@gmail.com>
2025-02-06 19:49:33 +0800

    feat: add index.html

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [ ! -d "iptv-sources" ]; then
-  git clone https://github.com/HerbertHe/iptv-sources.git
+  git clone https://github.com/HerbertHe/iptv-sources.git --depth 1
 fi
 cd iptv-sources
-git pull
 yarn install
+yarn build
 yarn m3u
+cp -f index.html iptv-sources/m3u/
