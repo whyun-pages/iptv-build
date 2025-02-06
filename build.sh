f57e384a02bec1c2c595ba40cdcae5c38d1f7621
yunnysunny <yunnysunny@gmail.com>
2025-02-06 19:56:20 +0800

    ci: use yarn 1.x to build

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ if [ ! -d "iptv-sources" ]; then
   git clone https://github.com/HerbertHe/iptv-sources.git --depth 1
 fi
 cd iptv-sources
+corepack enable
+corepack prepare yarn@1.22.19 --activate
 yarn install
 yarn build
 yarn m3u
